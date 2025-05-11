83c5612736a778cc56f74c362c23428a36ec0bd4
AndreyBykov <aabykov@gmail.com>
2025-05-12 00:27:04 +0200

    misc: put classnames to the same line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <nav
-                    className="
-                        supports-backdrop-blur:bg-background/60
-                        fixed left-0 right-0 top-0 z-20
-                        border-b bg-background/95 backdrop-blur
-                        w-full flex py-2.5 px-5 justify-between
-                    "
-                >
+                <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
                     <div>
                         <Link href={homePath()} className="text-lg font-bold">
                             Home
@@ -42,15 +35,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                         </Link>
                     </div>
                 </nav>
-                <main
-                    className="
-                        min-h-screen flex-1
-                        overflow-y-auto overflow-x-hidden
-                        py-24 px-8
-                        bg-secondary/20
-                        flex flex-col
-                    "
-                >
+                <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
                     {children}
                 </main>
             </body>
