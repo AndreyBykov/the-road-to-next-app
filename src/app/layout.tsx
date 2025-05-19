643192cfd53be4a761667592d49d06ea959d2059
AndreyBykov <aabykov@gmail.com>
2025-05-20 00:19:59 +0200

    feat: add button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
 const geistSans = Geist({
@@ -23,14 +24,21 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
+                <nav
+                    className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
                     <div>
-                        <Link href={homePath()} className="text-lg font-bold">
+                        <Link
+                            href={homePath()}
+                            className={buttonVariants({ variant: 'outline' })}
+                        >
                             Home
                         </Link>
                     </div>
                     <div>
-                        <Link href={ticketsPath()} className="text-sm underline">
+                        <Link
+                            href={ticketsPath()}
+                            className={buttonVariants({ variant: 'outline' })}
+                        >
                             Tickets
                         </Link>
                     </div>
